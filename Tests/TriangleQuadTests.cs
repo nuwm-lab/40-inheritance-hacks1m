1cddb0fd216cf7d1b03ea99c6dd00a021db78eac
hacks1m <maksim.balitskiy.19@gmail.com>
2025-10-28 15:04:13 +0200

    було перероблено програму під правильний варіант та видалено зайві файли

--- Tests/TriangleQuadTests.cs
+++ Tests/TriangleQuadTests.cs
@@ -1,5 +1,4 @@
 using System;
-using LabWork.Geometry;
 using Xunit;
 
 namespace LabWork.Tests
@@ -9,42 +8,22 @@ namespace LabWork.Tests
         [Fact]
         public void TriangleArea_HappyPath()
         {
-            var a = new Point(0, 0);
-            var b = new Point(1, 0);
-            var c = new Point(0, 1);
+            var a = new Point { X = 0, Y = 0 };
+            var b = new Point { X = 1, Y = 0 };
+            var c = new Point { X = 0, Y = 1 };
             var t = new Triangle(a, b, c);
-            Assert.Equal(0.5, t.Area(), 6);
-        }
-
-        [Fact]
-        public void Triangle_Degenerate_Throws()
-        {
-            var a = new Point(0, 0);
-            var b = new Point(1, 1);
-            var c = new Point(2, 2);
-            Assert.Throws<ArgumentException>(() => new Triangle(a, b, c));
+            Assert.Equal(0.5, t.CalculateArea(), 6);
         }
 
         [Fact]
         public void QuadrilateralArea_HappyPath()
         {
-            var p1 = new Point(0, 0);
-            var p2 = new Point(1, 0);
-            var p3 = new Point(1, 1);
-            var p4 = new Point(0, 1);
+            var p1 = new Point { X = 0, Y = 0 };
+            var p2 = new Point { X = 1, Y = 0 };
+            var p3 = new Point { X = 1, Y = 1 };
+            var p4 = new Point { X = 0, Y = 1 };
             var q = new ConvexQuadrilateral(p1, p2, p3, p4);
-            Assert.Equal(1.0, q.Area(), 6);
-        }
-
-        [Fact]
-        public void Quadrilateral_NonConvex_Throws()
-        {
-            // self-intersecting (bow) quadrilateral
-            var p1 = new Point(0, 0);
-            var p2 = new Point(1, 1);
-            var p3 = new Point(0, 1);
-            var p4 = new Point(1, 0);
-            Assert.Throws<ArgumentException>(() => new ConvexQuadrilateral(p1, p2, p3, p4));
+            Assert.Equal(1.0, q.CalculateArea(), 6);
         }
     }
 }
